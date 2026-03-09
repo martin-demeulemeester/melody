4fdccc01a15c0316344a69f1016cf4ead020fc1d
martin-demeulemeester <martin.dml779@gmail.com>
2026-03-09 20:21:58 +0100

    FastApi first try

--- download_music_.py
+++ download_music_.py
@@ -29,10 +29,16 @@ if ffmpeg_location:
 
 def download_from_youtube(link):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([link])
+        ydl.download(link)
     print('done')
 
 
 
 if __name__ == '__main__':
-    download_from_youtube('https://youtu.be/l9MSAwb3YfY?si=1GfULdi7DCrAibg6')
+    liste = ['https://youtu.be/l9MSAwb3YfY?si=1GfULdi7DCrAibg6',
+             'https://youtu.be/_LUFMHvvNt4?si=XUXFrgPc0NdKjlpG',
+             'https://youtu.be/atgjKEgSqSU?si=1UC0ISLEoaF7rQfz',
+             'https://youtu.be/6pV-Qm0o6Rg?si=ss9nKmCIIKaTJbIg',
+             'https://youtu.be/7DjspOrDTUk?si=JQwg_v4O5x7NZ5Yf',
+             'https://youtu.be/guN5p5ALnjE?si=Xtuf24R4lAA2Y0Vk']
+    download_from_youtube(liste)
